3689b9321b1e78816b482304e8f5db6badd44529
Muhammad Shah Nawaz <shahnawaz.swz1202@gmail.com>
2026-07-31 18:49:18 +0500

    feat: enhance user registration with email existence check and improve password hashing

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,14 +1,44 @@
 const userModel = require('../models/user.model')
+const jwt = require('jsonwebtoken')
 
 
 /**
- * ? User register Controller
- * ! /api/auth/register
+ * - User register Controller
+ * - /api/auth/register
  */
-const userRegisterController = (req,res) =>{
-    const {email,name,password} = req.body
-    
+const userRegisterController = async (req, res) => {
+    const { email, name, password } = req.body
 
+    const isExist = await userModel.findOne({ email: email })
+    if (isExist) {
+        return res.status(422).json({
+            message: "User with email Already Exists.",
+            status: 'Failed.'
+        })
+    }
+
+    const user = await userModel.create({
+        email,
+        name,
+        password
+    })
+
+    const token  = jwt.sign({userId: user._id},process.env.JWT_SECRET)
+    res.cookie("jwt_token",token)
+    res.status(201).json({
+        user : {
+            _id: user._id,
+            email: user.email,
+            name: user.name
+        },
+        token
+    })
 
 }
 
+
+
+/** --------------------------------------------------------------------------------------------------------------------------- */
+module.exports = {
+    userRegisterController
+}
--- src/models/user.model.js
+++ src/models/user.model.js
@@ -18,8 +18,8 @@ const userSchema = new mongoose.Schema({
     password: {
         type: String,
         required: [true, 'Password is required'],
-        match: [/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
-            'Password must be at least 8 characters long and contain at least one uppercase letter, one lowercase letter, one number, and one special character.'],
+        // match: [/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
+        //     'Password must be at least 8 characters long and contain at least one uppercase letter, one lowercase letter, one number, and one special character.'],
         minlength: [6, "Password should be atleast 6 characters"],
         select: false,
     }
@@ -27,21 +27,20 @@ const userSchema = new mongoose.Schema({
     timestamps : true
 })
 /**----------------------------------------------------------------------------------------------------------------------- */
-userSchema.pre("save", async function (next) {
-    if(!this.isModified("password")){
-        return next()
+userSchema.pre("save", async function () {
+    if (!this.isModified("password")) {
+        return;
     }
-    const hash = await bcrypt.hash(this.password,11)
-    this.password = hash
-    return next()
-    
-})
+
+    this.password = await bcrypt.hash(this.password, 11);
+    return
+});
 /**----------------------------------------------------------------------------------------------------------------------- */
-userSchema.methods.comparePassword = async (password) => {
+userSchema.methods.comparePassword = async function (password)  {
     return await bcrypt.compare(password,this.password)
 }
 /**----------------------------------------------------------------------------------------------------------------------- */
-const userModel = moongose.model("user",userSchema)
+const userModel = mongoose.model("user",userSchema)
 
 module.exports = userModel 
 
