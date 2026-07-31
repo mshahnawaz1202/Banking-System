bc012ceeefa7aab073c4a7532b15c6dbf36b1e04
Muhammad Shah Nawaz <shahnawaz.swz1202@gmail.com>
2026-07-31 16:42:52 +0500

    feat: add user model with validation and pre-save hook

---
+++ src/models/user.model.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+
+
+
+const userSchema = mongoose.Schema({
+    email: {
+        type: String,
+        required: [true, 'Email address is required'],
+        unique: [true, "Email Already Exists!"],
+        lowercase: true,
+        trim: true,
+        match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address']
+    },
+    name: {
+        type: String,
+        required: [true, 'Name is required'],
+
+    },
+    password: {
+        type: String,
+        required: [true, 'Password is required'],
+        match: [/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
+            'Password must be at least 8 characters long and contain at least one uppercase letter, one lowercase letter, one number, and one special character.'],
+        minlength: [6, "Password should be atleast 6 characters"],
+        select: false,
+    }
+},{
+    timestamps : true
+})
+
+userSchema.pre("save", async function (next) {
+    
+    
+})
+
+
+
