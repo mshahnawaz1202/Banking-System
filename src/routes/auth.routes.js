a3e4e8b87c301490415a03d1dbc87bc9c86135df
Muhammad Shah Nawaz <shahnawaz.swz1202@gmail.com>
2026-07-31 17:47:21 +0500

    feat: link user registration route to the controller

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,12 +1,12 @@
 const express = require("express");
-
+const authController = require('../controllers/auth.controller')
 const router = express.Router()
 
 
 /**
  * * Post /api/auth/register
  */
-router.post('/register',)
+router.post('/register',authController.userRegisterController)
 
 
 
