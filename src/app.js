f9532cb9967e1b00d8e09411c07920e2faba69df
Muhammad Shah Nawaz <shahnawaz.swz1202@gmail.com>
2026-07-31 18:49:03 +0500

    feat: add cookie-parser middleware for enhanced cookie management

--- src/app.js
+++ src/app.js
@@ -1,9 +1,12 @@
 
 const express = require("express");
+const cookieParser = require('cookie-parser')
 const authRoute = require('./routes/auth.routes')
 app = express()
 
 app.use(express.json());
+app.use(cookieParser())
+
 
 app.use('/api/auth',authRoute)
 
