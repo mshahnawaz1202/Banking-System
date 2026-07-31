8fdd020bff41f3602aca8a9ba8c8c39a503e8787
Muhammad Shah Nawaz <shahnawaz.swz1202@gmail.com>
2026-07-31 17:32:58 +0500

    feat: implement user registration controller

---
+++ src/controllers/auth.controller.js
@@ -0,0 +1,14 @@
+const userModel = require('../models/user.model')
+
+
+/**
+ * ? User register Controller
+ * ! /api/auth/register
+ */
+const userRegisterController = (req,res) =>{
+    const {email,name,password} = req.body
+    
+
+
+}
+
