9278863e42d1dfcb38f866682c97c8852cdd7563
Muhammad Shah Nawaz <shahnawaz.swz1202@gmail.com>
2026-08-01 19:08:57 +0500

    feat: implement user login controller with authentication logic

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -37,8 +37,51 @@ const userRegisterController = async (req, res) => {
 }
 
 
+/**
+ * - User login Controller
+ * - /api/auth/login
+ */
+const userLoginController = async (req,res) => {
+    const {email, password} = req.body
+    /**
+     * ? Firstly Find user with email
+     * ! if user not exists send a 401 response, email or password id invalid
+     * * if user exists then check password
+     * ! if password is invalid then again send a 401 response, email or password is incorrect
+     * * if password is valid then again generate a token
+     */
+    const user = await userModel.findOne({
+        email:email
+    }).select("+password")
+    
+    if(!user){
+        return res.status(401).json({
+            message : "Email or Password is Invalid!"
+        })
+    }
+    const isValidPassword = await user.comparePassword(password)
+    if(!isValidPassword){
+        return res.status(401).json({
+            message : "Email or Password is Invalid!"
+        })
+    }
+
+    const token  = jwt.sign({userId: user._id},process.env.JWT_SECRET)
+    res.cookie("jwt_token",token)
+    res.status(200).json({
+        user : {
+            _id: user._id,
+            email: user.email,
+            name: user.name
+        },
+        token
+    })
+}
+
+
+
 
 /** --------------------------------------------------------------------------------------------------------------------------- */
 module.exports = {
-    userRegisterController
+    userRegisterController,userLoginController
 }
