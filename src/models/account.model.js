e6652849c89d828e9a99fd5c91c362b5f679a0b8
Muhammad Shah Nawaz <shahnawaz.swz1202@gmail.com>
2026-08-02 13:59:26 +0500

    fix: correct formatting of status field in account schema

--- src/models/account.model.js
+++ src/models/account.model.js
@@ -9,11 +9,13 @@ const accountSchema = new mongoose.Schema({
     },
 
     status: {
+        type:String,
         enum: {
             values: ["ACTIVE", "FROZEN", "CLOSED"],
             message: "Status can be either ACTIVE, FROZEN, CLOSED.",
-            default : "ACTIVE"
-        }
+            
+        },
+        default : "ACTIVE"
     },
 
     currency: {
