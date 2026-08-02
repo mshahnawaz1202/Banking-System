32f97f755b318e771e461ef9dd86667d4ccaa34b
Muhammad Shah Nawaz <shahnawaz.swz1202@gmail.com>
2026-08-02 13:31:13 +0500

    fix: correct route assignment for account creation

--- src/app.js
+++ src/app.js
@@ -1,14 +1,31 @@
-
+/**
+ * ! Modules required
+ */
 const express = require("express");
 const cookieParser = require('cookie-parser')
+
+/**
+ * ? Routes
+ */
 const authRoute = require('./routes/auth.routes')
+const accountRoute = require('./routes/account.routes')
+
+
 app = express()
 
+
+/** Middlewares */
 app.use(express.json());
 app.use(cookieParser())
 
-
+/** 
+ * ? use Routes
+ * * /api/auth => login and resgiteration
+ * * /api/account => account creation
+ * 
+ */
 app.use('/api/auth',authRoute)
+app.use('/api/account',authRoute)
 
 
 
