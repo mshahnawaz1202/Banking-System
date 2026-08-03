440dda0973f782b8748d3d68340f381002a476cf
Muhammad Shah Nawaz <shahnawaz.swz1202@gmail.com>
2026-08-03 15:55:20 +0500

    fix: correct spelling of transaction field in ledger model

--- src/models/ledger.model.js
+++ src/models/ledger.model.js
@@ -17,7 +17,7 @@ const ledgerSchema = new mongoose.Schema({
         immutable: true,
     },
 
-    transcation: {
+    transaction: {
         type: mongoose.Schema.Types.ObjectId,
         ref: "transaction",
         required: [true, "Ledger must be associated with a transaction!"],
@@ -35,11 +35,33 @@ const ledgerSchema = new mongoose.Schema({
         immutable: true
     }
 
+}, {
+    timestamps: true
 })
 
+/**
+ * * function to prevent ledger from modifications
+ */
 
+const preventLedgerModifications = () => {
+    throw new Error("Ledger enteries are immutable and cannot be modified or deleted!")
+}
 
+/**
+ * * whenever someone trying to update or delete ledger we call above function
+ */
 
+/** prevent updates */
+
+ledgerSchema.pre("findOneAndUpdate", preventLedgerModifications);
+ledgerSchema.pre("updateOne", preventLedgerModifications);
+ledgerSchema.pre("updateMany", preventLedgerModifications);
+
+/** prevent delete  */
+
+ledgerSchema.pre("findOneAndDelete", preventLedgerModifications);
+ledgerSchema.pre("deleteOne", preventLedgerModifications);
+ledgerSchema.pre("deleteMany", preventLedgerModifications);
 
 
 
