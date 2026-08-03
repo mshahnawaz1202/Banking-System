5c822e654b47666be1141e700fd9e326712bb46b
Muhammad Shah Nawaz <shahnawaz.swz1202@gmail.com>
2026-08-03 15:21:41 +0500

    feat: add transaction reference and type fields to ledger model

--- src/models/ledger.model.js
+++ src/models/ledger.model.js
@@ -15,6 +15,24 @@ const ledgerSchema = new mongoose.Schema({
         required: [true, "Amount is required for a Ledger Entry!"],
         index: true,
         immutable: true,
+    },
+
+    transcation: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "transaction",
+        required: [true, "Ledger must be associated with a transaction!"],
+        index: true,
+        immutable: true,
+    },
+
+    type: {
+        type: String,
+        enum: {
+            values: ["CREDIT", "DEBIT"],
+            message: "Type can be either credit or debit"
+        },
+        required: [true, "Ledger type is required"],
+        immutable: true
     }
 
 })
