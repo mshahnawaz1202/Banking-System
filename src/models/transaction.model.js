a5d798f24f938bc6db7f64a9d09bf578a3406971
Muhammad Shah Nawaz <shahnawaz.swz1202@gmail.com>
2026-08-03 12:24:49 +0500

    fix: correct formatting of status field in transaction schema

--- src/models/transaction.model.js
+++ src/models/transaction.model.js
@@ -18,15 +18,15 @@ const transactionSchema = new mongoose.Schema({
         index: true,
     },
 
-    status : {
-        type:String,
-        enum : {
-            values:["PENDING", "COMPLETED", "FAILED", "REVERSED"],
-            message : "Status can be either PENDING,COMPLETED, FAILED or REVERSED."
+    status: {
+        type: String,
+        enum: {
+            values: ["PENDING", "COMPLETED", "FAILED", "REVERSED"],
+            message: "Status can be either PENDING,COMPLETED, FAILED or REVERSED."
         },
         default: "PENDING",
     },
-    
+
 
 
 })
