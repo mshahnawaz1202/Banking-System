75bcb05c917d3dc066857403460140534e5efd29
Muhammad Shah Nawaz <shahnawaz.swz1202@gmail.com>
2026-08-04 19:18:00 +0500

    feat: add route for initial funds transaction from system user with authentication middleware

--- src/routes/transaction.routes.js
+++ src/routes/transaction.routes.js
@@ -11,4 +11,12 @@ const transactionRoutes = Router()
 transactionRoutes.post('/',authMiddleware.authMiddleware,transactionController.createTransaction)
 
 
+/**
+ * * - POST /api/transaction/system/initial-funds
+ * * - Create intitial funds transaction from system user
+ */
+transactionRoutes.post('/system/initial-funds',authMiddleware.authSystemUserMiddleware,transactionController.createTransaction)
+
+
+
 module.exports = transactionRoutes
