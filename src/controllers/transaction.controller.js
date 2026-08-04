fbf0dded2cd22385a1736d6db459be3f484c1c98
Muhammad Shah Nawaz <shahnawaz.swz1202@gmail.com>
2026-08-04 19:52:56 +0500

    feat: implement createInitialFundsTransaction for system users with error handling

--- src/controllers/transaction.controller.js
+++ src/controllers/transaction.controller.js
@@ -1,3 +1,14 @@
+
+
+
+const mongoose = require("mongoose");
+const transactionModel = require('../models/transaction.model')
+const ledgerModel = require('../models/ledger.model')
+const accountModel = require('../models/account.model')
+const emailService = require('../services/email.service')
+
+/**-----------------------------------------Creating a traansaction for a user--------------------------------------------------------------------------------------- */
+
 /**
  * ? Create a New Transaction
  *
@@ -36,22 +47,6 @@
  * * 10. Send Email Notification
  * *     - Notify sender and receiver of the successful transfer.
  */
-//! FAILURE HANDLING
-
-/**
- * * If any step fails:
- * * - Abort MongoDB transaction
- * * - Rollback all database changes
- * * - Mark transaction as FAILED
- * * - Return appropriate error response
- */
-
-const mongoose = require("mongoose");
-const transactionModel = require('../models/transaction.model')
-const ledgerModel = require('../models/ledger.model')
-const accountModel = require('../models/account.model')
-const emailService = require('../services/email.service')
-
 
 async function createTransaction(req, res) {
 
@@ -207,6 +202,74 @@ async function createTransaction(req, res) {
     })
 }
 
+/** ---------------------------------------------------Create initial funds transaction------------------------------------------------------------------------------------ */
+async function createInitialFundsTransaction(req,res){
+    const {toAccount, amount, idempotencyKey} = req.body
+
+    if(!toAccount || !amount || idempotencyKey){
+        return res.status(400).json({
+            message: "toAccount, amount and idempotencyKey are required!"
+        })
+    }
+
+    const toUserAccount = await accountModel.findOne({
+        _id: toAccount,
+    })
+
+    if (! !toUserAccount) {
+        return res.status(400).json({
+            message: "Invalid toUserAccount."
+        })
+    }
+
+     const fromUserAccount = await accountModel.findOne({
+        systemUser: true,
+        user: req.user._id
+    })
+
+    if(!fromUserAccount){
+        return res.status(400).json({
+            message: "Systme user account not found."
+        })
+    }
+    const session = await mongoose.startSession()
+    session.startTransaction()
+
+
+    const transaction = await transactionModel.create({
+        fromAccount: fromUserAccount._id,
+        toAccount,
+        amount,
+        idempotencyKey,
+        status: "PENDING",
+    }, { session })
+    const debitLedgerEntry = await ledgerModel.create({
+        account: fromAccount,
+        amount: amount,
+        transaction: transaction._id,
+        type: "DEBIT",
+
+    }, { session })
+
+    const creditLedgerEntry = await ledgerModel.create({
+        account: toAccount,
+        amount: amount,
+        transaction: transaction._id,
+        type: "CREDIT",
+
+    }, { session })
+
+    transaction.status = "COMPLETED"
+    await transaction.save({ session })
+
+    await session.commitTransaction()
+    session.endSession()
+    res.status(201).json({
+        message:"Initial Funds Transaction Completed Successfully!",
+        transaction: transaction
+    })
+}
+
 module.exports = {
-    createTransaction
+    createTransaction,createInitialFundsTransaction
 }
