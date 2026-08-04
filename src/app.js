12f5e87f897fcc21d830b698a554c626f625201f
Muhammad Shah Nawaz <shahnawaz.swz1202@gmail.com>
2026-08-04 14:54:12 +0500

    refactor: clean up app.js for improved readability and consistency

--- src/app.js
+++ src/app.js
@@ -2,36 +2,29 @@
  * ! Modules required
  */
 const express = require("express");
-const cookieParser = require('cookie-parser')
+const cookieParser = require("cookie-parser");
 
 /**
  * ? Routes
  */
-const authRoute = require('./routes/auth.routes')
-const accountRoute = require('./routes/account.routes')
+const authRoute = require("./routes/auth.routes");
+const accountRoute = require("./routes/account.routes");
+const transactionRoute = require("./routes/transaction.routes");
 
-
-app = express()
-
+const app = express();
 
 /** Middlewares */
 app.use(express.json());
-app.use(cookieParser())
+app.use(cookieParser());
 
-/** 
- * ? use Routes
- * * /api/auth => login and resgiteration
- * * /api/account => account creation
- * 
+/**
+ * ? Use Routes
+ * * /api/auth        => User registration & login
+ * * /api/account     => Account management
+ * * /api/transaction => Money transfers
  */
-app.use('/api/auth',authRoute)
-app.use('/api/account',accountRoute)
+app.use("/api/auth", authRoute);
+app.use("/api/account", accountRoute);
+app.use("/api/transaction", transactionRoute);
 
-
-
-
-
-
-
-
-module.exports = app
+module.exports = app;
