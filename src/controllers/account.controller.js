069b84a1e2b372d5f072d49d91bfe3d126ea40fe
Muhammad Shah Nawaz <shahnawaz.swz1202@gmail.com>
2026-08-05 21:34:30 +0500

    feat: add account management controllers for retrieving user accounts and balance

--- src/controllers/account.controller.js
+++ src/controllers/account.controller.js
@@ -1,6 +1,6 @@
 const accountModel = require('../models/account.model')
 
-async function createAccountController(req,res){
+async function createAccountController(req, res) {
     const user = req.user
     const account = await accountModel.create({
         user: user._id,
@@ -13,8 +13,40 @@ async function createAccountController(req,res){
 }
 
 
+async function getAllUsersAccountController(req, res) {
+    const accounts = await accountModel.find({
+        user: req.user._id,
+    })
+
+    res.status(200).json({
+        accounts,
+
+    })
+}
+
+
+async function getAccountBalanceController(req, res) {
+    const { accountId } = req.params
+    const account = await accountModel.findOne({
+        _id: accountId,
+        user: req.user._id
+    })
+
+    if (!account) {
+        res.status(404).json({
+            message: "Account Not found"
+        })
+    }
+
+    const balance = await account.getBalance();
+    res.status(200).json({
+        accountId: account._id,
+        balance: balance
+    })
+}
+
 module.exports = {
-    createAccountController
+    createAccountController, getAllUsersAccountController, getAccountBalanceController,
 }
 
 
